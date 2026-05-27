4d07619164f13253847fb9e1df712e33b3c65dbc
indrol12 <indrolofficial@gmail.com>
2026-05-27 13:59:21 +0530

    polymorphism completed successfully

--- 11may_csharpprgms/hierarchical_inheritance.cs
+++ 11may_csharpprgms/hierarchical_inheritance.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Channels;
+//using System.Threading.Tasks;
 
-class Maharashtra()
-{
-    public void mh()
-    {
-        Console.WriteLine("Best state in India");
-    }
-}
-class pune:Maharashtra
-{
-    public void speciality()
-    {
-        Console.WriteLine("Pune is the best city for live ");
-    }
-}
-class Mumbai:Maharashtra
-{
-    public void industry()
-    {
-        Console.WriteLine("Mumbai is famous for vadapav and traffic");
-    }
-}
-   class hierarchical_inheritance
-    {
-    public static void Main(string[] args)
-    {
-        pune p = new pune();
-        p.mh();
-        p.speciality();
+//class Maharashtra()
+//{
+//    public void mh()
+//    {
+//        Console.WriteLine("Best state in India");
+//    }
+//}
+//class pune:Maharashtra
+//{
+//    public void speciality()
+//    {
+//        Console.WriteLine("Pune is the best city for live ");
+//    }
+//}
+//class Mumbai:Maharashtra
+//{
+//    public void industry()
+//    {
+//        Console.WriteLine("Mumbai is famous for vadapav and traffic");
+//    }
+//}
+//   class hierarchical_inheritance
+//    {
+//    public static void Main(string[] args)
+//    {
+//        pune p = new pune();
+//        p.mh();
+//        p.speciality();
 
-        Mumbai m=new Mumbai();
-        m.industry();
-    }
-    }
+//        Mumbai m=new Mumbai();
+//        m.industry();
+//    }
+//    }
 
---
+++ 11may_csharpprgms/methodoverloading.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+//     class methodoverloading
+//    {
+
+//    void bill_payment()//1,passing no paramter
+//    {
+//        Console.WriteLine("Bill payment of electricity bill");
+//    }
+//    void bill_payment(int amount)//2 passing parameter
+//    {
+//        Console.WriteLine("Bill payment of waterbill");
+//    }
+//    void bill_payment(int a,string b)//3 passing number of parameter
+//    {
+//        Console.WriteLine("Bill payment of telephone bill");
+//    } 
+//    void bill_payment(string name)//4 by changing the datatype of parameter
+//    {
+//        Console.WriteLine("Bill payment of credit card bill");
+//    }
+//    void bill_payment(string name,int a)//5 by changing the sequence of parameter
+//    {
+//        Console.WriteLine("Bill payment of gas bill");
+//    }
+    
+//}
